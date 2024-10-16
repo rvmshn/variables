f8976e483bca5779334d7ce531c0bfd6320ced3b
Ruslan <ruslan95891@gmail.com>
2024-10-16 21:20:22 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+exercises_count = 12
+hours_count = 1.5
+training_course = 'Python'
+single_exercise_timer = (hours_count / exercises_count)
+print('Курс:', training_course,', всего задач: ', exercises_count,', затрачено часов:',hours_count,', среднее время выполнения', single_exercise_timer,'часа.')
